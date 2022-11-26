9f464921598d98812156d2078ce20d4c049673ed
Anish Mohanty <108749478+Anish-Byte-Bot@users.noreply.github.com>
2022-11-26 15:12:28 +0530

    Create q4.java

---
+++ q4.java
@@ -0,0 +1,12 @@
+package demojava;
+
+public class q4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String p = "	My name is Anish	";
+		p = p.replaceAll("\\s", "");
+		System.out.println(p);
+		
+
+	}
