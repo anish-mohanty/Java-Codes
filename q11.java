7b9140f872e147a18a5b6668e4a0be628a1d9c3b
Anish Mohanty <108749478+Anish-Byte-Bot@users.noreply.github.com>
2022-11-26 15:21:11 +0530

    Create q11.java

---
+++ q11.java
@@ -0,0 +1,18 @@
+package demojava;
+
+public class q11 {
+
+	public static void main(String[] args) {
+		String[] a1 = new String[] {"qwerty"};   
+		String[] a2 = new String[] {"qqwweerrttyy"};   
+	 
+		if (a1 == a2)   
+		System.out.println("Arrays are equal.");   
+		else  
+		System.out.println("Arrays are not equal.");   
+		}   
+		
+
+	}
+
+
