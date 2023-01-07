a0f011f7f9c123dcf8d9cdec51dd597a6a5f60c7
Anish Mohanty <108749478+Anish-Byte-Bot@users.noreply.github.com>
2023-01-07 14:57:03 +0530

    Create p1.java

---
+++ csw book ch 16/p1.java
@@ -0,0 +1,12 @@
+package lab2;
+
+public class p1 {
+	
+public class LiteralExample {
+	 public static void main(String args[]) {
+	 String message = "Hello World! I love Java";
+	 System.out.println(message);
+	 }
+	} 
+
+}
