db1d67e7a5a936c9eb71cf26303708958d521ee8
Anish Mohanty <108749478+Anish-Byte-Bot@users.noreply.github.com>
2023-01-07 14:58:38 +0530

    Add files via upload

---
+++ csw book ch 16/p2.java
@@ -0,0 +1,10 @@
+package lab2;
+
+public class p2 {
+	public static void main(String args[]) {
+		 char[] javaArray = { 'I', ' ' , 'L', 'O', 'V', 'E', ' ' ,'J', 'A', 'V', 'A' };
+		 String javaString = new String(javaArray);
+		 System.out.println(javaString);
+		 }
+		}
+
---
+++ csw book ch 16/p3.java
@@ -0,0 +1,20 @@
+package lab2;
+
+public class p3 {
+	public static void main(String[] args) {
+		 // The following strings are being created using literals
+		 String literal1 = "xyz";
+		 String literal2 = "xyz"; 
+		 System.out.print("Comparison using == operator for literals : ");
+		 System.out.println(literal1 == literal2); // The output of this line of code will 
+	
+		 // The following two strings are being created using the new operator
+		 String keyword1 = new String("abc");
+		 String keyword2 = new String("abc");
+		 System.out.print("Comparison using == operator for objects : ");
+		 System.out.println(keyword1 == keyword2); // The output of this line of code will 
+		
+		 }
+		} 
+
+
