8488d0c4fa97d279ccbf5d0ebaddb60a330d4f2a
Anish Mohanty <108749478+Anish-Byte-Bot@users.noreply.github.com>
2023-01-13 12:01:29 +0530

    Add files via upload

---
+++ ch 13/LinkedHashSetExample.java
@@ -0,0 +1,31 @@
+package test1;
+import java.util.LinkedHashSet;
+public class LinkedHashSetExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedHashSet<String> lhset1 = new LinkedHashSet<String>();
+		
+		 lhset1.add("A");
+		 lhset1.add("N");
+		 lhset1.add("I");
+		 lhset1.add("S");
+		 lhset1.add("HHH");
+		 lhset1.add("MOH");
+		 System.out.println(lhset1);
+	
+		 LinkedHashSet<Integer> lhset2 = new LinkedHashSet<Integer>();
+		
+		 lhset2.add(85);
+		 lhset2.add(23);
+		 lhset2.add(0);
+		 lhset2.add(45);
+		 lhset2.add(23);
+		 lhset2.add(51);
+		 System.out.println(lhset2);
+		 }
+		
+
+
+	}
+
---
+++ ch 13/TreeSetExample.java
@@ -0,0 +1,23 @@
+package test1;
+import java.util.TreeSet;
+
+public class TreeSetExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TreeSet<String> tset = new TreeSet<String>();
+		 
+		 tset.add("QWE");
+		 tset.add("QWERTY");
+		 tset.add("T20");
+		 tset.add("Porkistan");
+		 tset.add("India");
+		 tset.add("GGWP");
+	
+		 System.out.println(tset);
+		 }
+		
+
+	}
+
+
