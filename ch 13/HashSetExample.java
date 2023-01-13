5150247f42b2a7dcf575def7f8502d50052df717
Anish Mohanty <108749478+Anish-Byte-Bot@users.noreply.github.com>
2023-01-13 11:47:34 +0530

    Add files via upload

---
+++ ch 13/HashSetExample.java
@@ -0,0 +1,23 @@
+package test1;
+import java.util.HashSet;
+public class HashSetExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashSet<String> haset = new HashSet<String>();
+		 
+		 haset.add("Apple");
+		 haset.add("Mango");
+		 haset.add("Orange");
+		 haset.add("Dates");
+		 haset.add("Strawberry");
+		 haset.add("Orange");
+		 haset.add("Mango");
+		 haset.add(null);
+		 haset.add(null);
+		 System.out.println(haset);
+		 }
+		
+	}
+
+
