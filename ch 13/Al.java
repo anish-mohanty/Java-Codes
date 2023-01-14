81fdbe0c3ac2d80643afa76bd6e4a499c7d931f6
Anish Mohanty <108749478+Anish-Byte-Bot@users.noreply.github.com>
2023-01-14 14:31:07 +0530

    Add files via upload

---
+++ ch 13/Al.java
@@ -0,0 +1,16 @@
+package practice;
+import java.util.ArrayList;
+public class Al {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList a=new ArrayList();
+		a.add("barry");
+		a.add(25);
+		a.add(20);
+		int n=(int)a.get(1);
+		System.out.println(n);
+
+	}
+
+}
